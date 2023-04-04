5498cfe577b6cfd759167c83a821f52d190eb4a7
Sally Poon <sallypoon@hotmail.com>
2023-04-04 02:05:05 -0700

    Add chakra provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-
+  <ChakraProvider>
     <Router>
       <App />
     </Router>
+  </ChakraProvider>
 );
 
