48a7d2be695fe35b684713e0503299cf043a3e2e
Sally Poon <sallypoon@hotmail.com>
2023-04-07 18:15:06 -0700

    Complete report for top users at every endpoint

---
+++ src/components/admin/TopEndpointUsersReport.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
+
+
+const TopEndpointUsersReport = ({ logs }) => {
+    return (
+
+            <TableContainer>
+                <Table variant='simple'>
+                    <TableCaption>Top User by Endpoint, of all time</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>Endpoint</Th>
+                            <Th>Top User</Th>
+                            <Th isNumeric>Requested Count</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {
+                            logs.map(log => {
+                                return (
+                                    <Tr>
+                                        <Td>{log['endpoint']}</Td>
+                                        <Td>{log['user']['username']}</Td>
+                                        <Td isNumeric>{log['user']['count']}</Td>
+                                    </Tr>
+                                )
+                            })
+                        }
+                    </Tbody>
+                </Table>
+            </TableContainer>
+    )
+};
+
+export default TopEndpointUsersReport;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -14,10 +14,12 @@ import axios from 'axios';
 import Navbar from '../components/layout/Navbar';
 import UniqueUsersReport from '../components/admin/UniqueUsersReport';
 import TopUsersOverallReport from '../components/admin/TopUsersOverallReport';
+import TopEndpointUsersReport from '../components/admin/TopEndpointUsersReport';
 
 const AdminPage = () => {
     const [uniqueUsers, setUniqueUsers] = useState([]);
     const [topUsersOverall, setTopUsersOverall] = useState([]);
+    const [topEndpointUsers, setTopEndpointUsers] = useState([]);
 
     const fetchUniqueUsers = async () => {
         let res = await axios.get('http://localhost:6001/adminReports/uniqueUsers', {
@@ -37,9 +39,19 @@ const AdminPage = () => {
         setTopUsersOverall(res.data);
     }
 
+    const fetchTopEndpointUsers = async () => {
+        let res = await axios.get('http://localhost:6001/adminReports/endpointTop', {
+            headers: {
+                'auth-token-access': localStorage.getItem('access_token')
+            }
+        });
+        setTopEndpointUsers(res.data);
+    }
+
     useEffect(() => {
       fetchUniqueUsers();
       fetchTopUsersOverall();
+      fetchTopEndpointUsers();
     }, [])
 
     return (
@@ -88,10 +100,7 @@ const AdminPage = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                            <TopEndpointUsersReport logs={topEndpointUsers}/>
                         </AccordionPanel>
                     </AccordionItem>
 
